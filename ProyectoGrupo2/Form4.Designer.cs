92e808b39cbec033b9a6d5e62d4dcfb75f7edbeb
IsaacP <119353170+IsaacPerugaOrtiga@users.noreply.github.com>
2023-02-14 11:05:22 +0100

    Cambio del diseño de la ventana
    He pegado el código que tengo en la ventana creada.

--- ProyectoGrupo2/Form4.Designer.cs
+++ ProyectoGrupo2/Form4.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ProyectoGrupo2
+namespace ProyectoGrupo2
 {
     partial class Form4
     {
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnMod = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbClientes = new System.Windows.Forms.ComboBox();
+            this.tbPago = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btnMod
+            // 
+            this.btnMod.Location = new System.Drawing.Point(568, 332);
+            this.btnMod.Name = "btnMod";
+            this.btnMod.Size = new System.Drawing.Size(75, 23);
+            this.btnMod.TabIndex = 0;
+            this.btnMod.Text = "Modificar";
+            this.btnMod.UseVisualStyleBackColor = true;
+            this.btnMod.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(160, 332);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(58, 56);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(96, 16);
+            this.lblCodigo.TabIndex = 2;
+            this.lblCodigo.Text = "Código cliente:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Forma de pago:";
+            // 
+            // cbClientes
+            // 
+            this.cbClientes.FormattingEnabled = true;
+            this.cbClientes.Location = new System.Drawing.Point(174, 56);
+            this.cbClientes.Name = "cbClientes";
+            this.cbClientes.Size = new System.Drawing.Size(121, 24);
+            this.cbClientes.TabIndex = 4;
+            // 
+            // tbPago
+            // 
+            this.tbPago.Location = new System.Drawing.Point(174, 124);
+            this.tbPago.Name = "tbPago";
+            this.tbPago.Size = new System.Drawing.Size(100, 22);
+            this.tbPago.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(476, 56);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.tbPago);
+            this.Controls.Add(this.cbClientes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnMod);
+            this.Name = "Form4";
             this.Text = "Form4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnMod;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbClientes;
+        private System.Windows.Forms.TextBox tbPago;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
